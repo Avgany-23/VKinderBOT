5e26c76cc479b1f6d14787881e350504a7459ecd
Neto nest <nike112200@yandex.ru>
2024-08-24 01:55:56 +0300

    update bot1

--- vk_bot/bot_function.py
+++ vk_bot/bot_function.py
@@ -1,6 +1,101 @@
-import vk_api
-from vk_api.longpoll import VkLongPoll, VkEventType
+from vk_api import VkApi, VkUpload, vk_api
 from vk_api.utils import get_random_id
-from vk_api import VkUpload  # Для отправки фотографий
-from vk_api.keyboard import VkKeyboard, VkKeyboardColor
+from typing import Optional
+from vk_api.keyboard import VkKeyboard
+from PIL import Image
+import json
 
+
+def send_message(vk: VkApi,
+                 user_id: int,
+                 message: str,
+                 keyboard: Optional[VkKeyboard] = None,
+                 template: str = None) -> None:
+    """Отправка сообщения ботом в чат"""
+    values = {
+        'user_id': user_id,
+        'message': message,
+        'random_id': get_random_id()
+    }
+    if keyboard:
+        values['keyboard'] = keyboard.get_keyboard()
+    if template:
+        values['template'] = template
+
+    vk.method('messages.send', values)
+
+
+def snow_snackbar(vk: VkApi, event_id: str, user_id: int, peer_id: int, event_data: str):
+    """Отправка всплывающего сообщения"""
+    values = {
+        'event_id': event_id,
+        'user_id': user_id,
+        'peer_id': peer_id,
+        'event_data': event_data
+    }
+    vk.method('messages.sendMessageEventAnswer', values)
+
+
+def list_users(count: int = 15, list_user: str = 'liked') -> str:
+    """
+    Возвращает count пользователей из БД.
+    list_user: like_pages - из таблицы LikedList, block_pages - из таблицы BlackList
+    """
+    if list_user == 'Like list':
+        return 'Список отмеченных пользователей: %s' % count
+    return 'Список игнорируемых пользователей: %s' % count
+
+
+def browsing_history(count):
+    """Из таблицы с историей просмотров достает последние count записей"""
+    return 'Ваша история просмотров: %s' % count
+
+
+def user_filters(id_user):
+    """Возвращает описание пользовательских фильтров"""
+    return f'Фильтры поиска:\n{id_user}'
+
+
+def get_photo_vk_id(path: str, vk: vk_api.VkApi) -> str:
+    """Получение vk-id фотографий"""
+    upload = VkUpload(vk)
+    upload_img = upload.photo_messages(photos=path)[0]
+    return '{}_{}'.format(upload_img['owner_id'], upload_img['id'])
+
+
+def correct_size_photo(path: str, width: int = 1300, height: int = 800) -> None:
+    """Изменение соотношение сторон, по умолчанию 13 к 8"""
+    with Image.open(path) as photo:
+        result = photo.resize((width, height))
+        result.save(path)
+
+
+def carousel_str(photo_id: list, label: str = 'Поставить лайк') -> str:
+    carousel_ = {"type": "carousel", "elements": []}
+    for photo in photo_id:
+        element = {
+            "photo_id": photo,
+            "action": {
+                "type": "open_photo"
+            },
+            "buttons": [{
+                "action": {
+                    "type": "text",
+                    "label": label,
+                    "payload": "{}"
+                }
+            }]
+        }
+        carousel_['elements'].append(element)
+    result = json.dumps(carousel_)
+
+    return result
+
+
+def get_template_carousel(vk, path) -> str:
+    result_photos = []
+    for el in path:
+        correct_size_photo(el)
+        result_photos.append(get_photo_vk_id(el, vk))
+
+    return carousel_str(result_photos)
---
+++ vk_bot/menu_button.py
@@ -0,0 +1,101 @@
+from vk_api.keyboard import VkKeyboard, VkKeyboardColor
+
+
+def main_menu() -> VkKeyboard:
+    """Inline клавиатура главного меню"""
+    keyboard = VkKeyboard(one_time=False)
+    keyboard.add_button('Найти половинку 💙',
+                                 color=VkKeyboardColor.SECONDARY,
+                                 payload={"type": "search_people"})
+    keyboard.add_line()
+    keyboard.add_button('Фильтры поиска людей',
+                                 color=VkKeyboardColor.PRIMARY,
+                                 payload={"type": "filters"})
+    keyboard.add_line()
+    keyboard.add_button('Like list',
+                        color=VkKeyboardColor.POSITIVE,
+                        payload={"type": "Like list"})
+    keyboard.add_button('Block list',
+                        color=VkKeyboardColor.NEGATIVE,
+                        payload={"type": "Block list"})
+    keyboard.add_line()
+    keyboard.add_button('История просмотров',
+                        color=VkKeyboardColor.SECONDARY,
+                        payload={"type": "browsing_history"})
+    keyboard.add_callback_button('Ваши отметки',
+                                 color=VkKeyboardColor.SECONDARY,
+                                 payload={"type": "show_snackbar", "text": "Вас отметило 0 человек, вы никому не нужны"})
+
+    return keyboard
+
+
+def filters_menu() -> VkKeyboard:
+    """Меню с выбором фильтров поиска"""
+    keyboard = VkKeyboard(one_time=False, inline=True)
+    keyboard.add_callback_button('Настройка фильтров, выберите параметры:',
+                                 color=VkKeyboardColor.PRIMARY,
+                                 payload={"type": "show_snackbar", "text": "Выберите настройки фильтров"})
+    keyboard.add_line()
+    keyboard.add_callback_button('Возраст:',
+                                 color=VkKeyboardColor.POSITIVE,
+                                 payload={"type": "show_snackbar", "text": "Выбор возраста"})
+    keyboard.add_button('14-18',
+                                 color=VkKeyboardColor.SECONDARY,
+                                 payload={"type": "14-18"})
+    keyboard.add_button('19-25',
+                                 color=VkKeyboardColor.SECONDARY,
+                                 payload={"type": "19-25"})
+    keyboard.add_button('26-35',
+                                 color=VkKeyboardColor.SECONDARY,
+                                 payload={"type": "26-35"})
+    keyboard.add_button('>35',
+                                 color=VkKeyboardColor.SECONDARY,
+                                 payload={"type": ">35"})
+    keyboard.add_line()
+    keyboard.add_callback_button('Пол:',
+                                 color=VkKeyboardColor.POSITIVE,
+                                 payload={"type": "show_snackbar", "text": "Выбор пола"})
+    keyboard.add_button('мужской',
+                                 color=VkKeyboardColor.SECONDARY,
+                                 payload={"type": "sex-male"})
+    keyboard.add_button('женский',
+                                 color=VkKeyboardColor.SECONDARY,
+                                 payload={"type": "sex-female"})
+    return keyboard
+
+
+def like_or_block_menu() -> VkKeyboard:
+    """Список отмеченных/заблокированных пользователей"""
+    keyboard = VkKeyboard(one_time=False, inline=True)
+    keyboard.add_callback_button('Тут будут фильтры',
+                                 color=VkKeyboardColor.SECONDARY,
+                                 payload={"type": "search_people"})
+    keyboard.add_line()
+    keyboard.add_button('Вернуться назад',
+                                 color=VkKeyboardColor.SECONDARY,
+                                 payload={"type": "main_menu"})
+    return keyboard
+
+
+def search_menu():
+    keyboard = VkKeyboard(one_time=False, inline=True)
+    keyboard.add_callback_button('Ссылка 1',
+                                 color=VkKeyboardColor.SECONDARY,
+                                 payload={"type": "open_link", 'link': 'https://vk-api.readthedocs.io/en/latest/bot_longpoll.html'})
+    keyboard.add_line()
+    keyboard.add_callback_button('Ссылка 2',
+                                 color=VkKeyboardColor.SECONDARY,
+                                 payload={"type": "open_link", 'link': 'https://vk-api.readthedocs.io/en/latest/bot_longpoll.html'})
+    return keyboard
+
+
+# def inline_main_menu() -> VkKeyboard:
+#     """Inline клавиатура главного меню"""
+#     keyboard = VkKeyboard(one_time=False, inline=True)
+#     keyboard.add_callback_button('Кнопка1', color=VkKeyboardColor.PRIMARY, payload={"type": "my_own_100500_type_edit"})
+#     keyboard.add_callback_button('Кнопка2', color=VkKeyboardColor.NEGATIVE, payload={"type": "open_link", "link": "https://vk.com/dev/bots_docs_5"})
+#     keyboard.add_callback_button('Кнопка3', color=VkKeyboardColor.PRIMARY, payload={"type": "open_link", "link": "https://vk.com/dev/bots_docs_5"})
+#     keyboard.add_callback_button('Добавить красного ', color=VkKeyboardColor.PRIMARY,
+#                                    payload={"type": "my_own_100500_type_edit"})
+#
+#     return keyboard
