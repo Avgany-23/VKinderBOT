ce643a0ac6159e6998ab2b69b1861d1d0982b883
anastasy_klimanova <klimanova_anastasy@mail.ru>
2024-08-27 17:26:24 +0300

    add tests

---
+++ api_vk/tests_vk_api/tests.py
@@ -0,0 +1,155 @@
+import unittest
+from unittest import TestCase
+from main import SearchVK
+from Redis_cash import clear_cache, update_cache, delete_user, find_in_cash_with_id
+
+
+api_token = ''
+vk_get = SearchVK(api_token)
+is_test = True
+user_id_1 = '119922158'
+user_id_2 = '49293108'
+
+class TestVK_api(TestCase):
+    #тест на выдачу ключей с динамической информацией
+    def test_get_user_info_vk(self):
+        result = vk_get.get_user_info_vk(user_id_1)
+        for info in result['response']:
+            self.assertIn('first_name', info)
+            self.assertIn('id', info)
+            self.assertIn('is_closed', info)
+            self.assertIn('last_name', info)
+            self.assertIn('sex', info)
+            self.assertIn('screen_name', info)
+
+    # тест на выдачу ключей с динамической информацией
+    def test_get_user_info_vk_2(self):
+        user_id_2 = '49293108'
+        result = vk_get.get_user_info_vk(user_id_2)
+        for info in result['response']:
+            self.assertIn('activities', info)
+            self.assertIn('first_name', info)
+            self.assertIn('can_access_closed', info)
+            self.assertIn('city', info)
+            self.assertIn('first_name', info)
+            self.assertIn('id', info)
+            self.assertIn('interests', info)
+            self.assertIn('is_closed', info)
+            self.assertIn('last_name', info)
+            self.assertIn('relation', info)
+            self.assertIn('screen_name', info)
+            self.assertIn('sex', info)
+
+    @unittest.skipIf(is_test, 'Необходим ключ пользователя для метода users.search')
+    # тест с неверным токеном
+    def test_get_users_vk(self):
+        api_token = '234ooo'
+        vk_get = SearchVK(api_token)
+        expected = [{'can_access_closed': True,
+                      'first_name': 'Александр',
+                      'id': 643640731,
+                      'is_closed': False,
+                      'last_name': 'Усс',
+                      'track_code': '8992299fCJabUuMQdv6cadnPZS2Hw9O5ducq-fhCny6ECcHK8lJv_41Xs3x1-5tm7CKwmWTDu7dw5Cr59iTtRw'},
+                     {'can_access_closed': True,
+                      'first_name': 'Алексей',
+                      'id': 434432823,
+                      'is_closed': False,
+                      'last_name': 'Кузьмин',
+                      'track_code': '46713ff0kw245fNzEJbxY74DcmwdgJ8RWe8lpRGlHt_fw4aHtTX0ZKy-rk1Aw_Zkj-yo2fyA9x9f7CWlH8Nstg'},
+                     {'can_access_closed': True,
+                      'first_name': 'Алексей',
+                      'id': 864559765,
+                      'is_closed': False,
+                      'last_name': 'Беспрозванных',
+                      'track_code': 'fdc3b9bacruYJHWtqPgktlLn9bl79UPLslZVj3djsqI_LBobU_4V0t53LpTyq3SxZAMgCJz1K8W0VVWPeQXAyw'},
+                     {'can_access_closed': True,
+                      'first_name': 'Виктор',
+                      'id': 646289221,
+                      'is_closed': False,
+                      'last_name': 'Томенко',
+                      'track_code': 'e54ec54eWh-ggFDx57_cXwmyPtirEmIqDj6psP-dWny7dUGK7YQ9draGAMrm799dNVbubUgSCiQIPamw8fsoFQ'},
+                     {'can_access_closed': True,
+                      'first_name': 'Василий',
+                      'id': 640646540,
+                      'is_closed': False,
+                      'last_name': 'Орлов',
+                      'track_code': '93a40d21KhPJ4c-ABsV0RxYT63yBqs0sVymgH3dRp1j1hd4l8G9Neou3xuwIxHFAJ_g8z2OqpSJRKqAfeTfVMQ'}]
+        result = vk_get.get_users_vk(5, sex=2, age_from=20, is_closed=False)
+        self.assertEqual(expected, result)
+
+    # тест на соответсвие заданного параметра count и получения пользователей
+    def test_get_users_vk_2(self):
+        expected = 6
+        result = vk_get.get_users_vk(6, sex=2, age_from=20, is_closed=False)
+        self.assertEqual(expected, len(result))
+
+    # тест на выдачу ключей с динамической информацией
+    def test_get_users_vk_3(self):
+        result = vk_get.get_users_vk(1, sex=2, age_from=20, is_closed=False)
+        for info in result:
+            self.assertIn('id', info)
+            self.assertIn('first_name', info)
+            self.assertIn('last_name', info)
+            self.assertIn('can_access_closed', info)
+            self.assertIn('track_code', info)
+
+    # тест на соответствие заданного is_closed=True и результата
+    def test_get_users_vk_4(self):
+        result = vk_get.get_users_vk(1, sex=2, age_from=20, is_closed=True)
+        for info in result:
+            if type(info) is dict:
+                a = info['can_access_closed']
+                self.assertTrue(a)
+
+    """уважаемые коллеги, функция get_photo_user возвращает в консоли для user_id_2="49293108":
+    Произошла ошибка, профиль пользователя закрыт,
+    Профиль у данного id действительно приватный
+    Тем не менее данный тест скачивает фото каким-то образом
+    были проведены: очиска кеша PC, браузера, открыт и закрыт профиль, безрезультатно
+    скиппим этот тест 
+    """
+    @unittest.skipIf(is_test, 'Невероятная работа API VK, просьба прочитать докстринг над тестом')
+    def test_get_photo_user(self):
+        expected = "Произошла ошибка, профиль пользователя закрыт"
+        result = vk_get.get_photo_user(user_id_2, place='profile', max_count=5)
+        self.assertEqual(expected, result)
+
+    # тест на соответствие заданного max_count=3 и результата
+    def test_get_photo_user_2(self):
+        result = vk_get.get_photo_user(user_id_1, place='profile', max_count=3)
+        for index, j in enumerate(result):
+            s = index
+        expected = s + 1
+        self.assertEqual(expected, len(result))
+
+    #проверка получения ссылок на фото пользователей
+    def test_get_photo_user_3(self):
+        result = vk_get.get_photo_user(user_id_1, place='profile', max_count=3)
+        for i in result.keys():
+            value = result[i]['photo_url']
+            expected = 'https://' in value
+            self.assertTrue(expected)
+
+
+class TestRedis_cash(TestCase):
+    #тест очистки кеша
+    def test_clear_cache(self):
+        result = clear_cache()
+        expected = 'Кэш очищен'
+        self.assertEqual(expected, result)
+
+    def test_update_cache(self):
+        result = update_cache()
+        expected = 'Кэш обновлен'
+        self.assertEqual(expected, result)
+
+    def find_in_cash_with_id(self):
+        result = find_in_cash_with_id(0)
+        expected = 'Пользователь не найден в кэше.'
+        self.assertEqual(expected, result)
+
+    def test_delete_user(self):
+        result = delete_user(864559765)
+        expected = 'Пользователь удален из Redis'
+        self.assertEqual(expected, result)
